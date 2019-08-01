257c14e1b32373757f9fd1a8b43f2d9c2e209e85
andreabecerrab <andii1wiki@gmail.com>
2019-08-01 18:25:20 -0500

    weather app init

---
+++ src/app.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+const publicDir = path.join(__dirname, '../public');
+const viewPaths = path.join(__dirname, '../templates/views');
+const partialPaths = path.join(__dirname, '../templates/partials');
+
+//geocode and forecast
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+//setup handlerbars engine views directory
+app.set('view engine', 'hbs');
+app.set('views', viewPaths);
+hbs.registerPartials(partialPaths);
+
+//setup public dir
+app.use(express.static(publicDir));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Andrea'
+  });
+});
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'Weather App',
+    name: 'Andrea'
+  });
+});
+
+app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({ error: 'No addres sended' });
+  }
+
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+      forecast(latitude, longitude, (error, data2) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: data2,
+          location: location,
+          address: req.query.address
+        });
+      });
+    }
+  );
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('noexists');
+});
+app.get('*', (req, res) => {
+  res.render('404');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require('request');
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    'https://api.darksky.net/forecast/3e255eb7e8285ddc741a4e8cfec3aa4e/' +
+    latitude +
+    ',' +
+    longitude +
+    '?units=si';
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to location services', undefined);
+    } else if (body.error) {
+      callback('no', undefined);
+    } else {
+      callback(undefined, {
+        temp: body.currently.temperature,
+        rain: body.currently.precipProbability
+      });
+    }
+  });
+};
+
+module.exports = forecast;
